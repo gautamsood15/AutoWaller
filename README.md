bf8f5024cc7835ed3049d8616503f18c08895b0e
gautamsood15 <gauty22@gmail.com>
2021-02-19 15:32:21 +0530

    Adding README data

---
+++ README.md
@@ -0,0 +1,7 @@
+# AutoWaller
+
+Autowaller is a python script written for cahning linux desktop background for every phase of the daty from morning to evening.
+
+The script is written is python and uses datetime and os module for finding time and executing the command -- gsettings to change the wallpaper 
+
+sample linux command --> gsettings set org.gnome.desktop.background picture-uri file:///home/gautamsood/Pictures/Wallpapers/early_night.jpg
--- autowaller.py
+++ autowaller.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 import os 
 from datetime import datetime
@@ -17,7 +17,7 @@ elif 8 > hr >= 5:
 elif 12 > hr >= 8:
 	os.system('gsettings set org.gnome.desktop.background picture-uri file:///home/gautamsood/Pictures/Wallpapers/late_morning.jpg')
 elif 18 >= hr >= 12:
-	os.system('gsettings set org.gnome.desktop.background picture-uri file:///home/gautamsood/Pictures/Wallpapers/afternoon.jpg')
+	os.system("gsettings set org.gnome.desktop.background picture-uri file:///home/gautamsood/Pictures/Wallpapers/afternoon.jpg")
 elif 23 >= hr > 18:
 	os.system('gsettings set org.gnome.desktop.background picture-uri file:///home/gautamsood/Pictures/Wallpapers/early_night.jpg')
 else:
