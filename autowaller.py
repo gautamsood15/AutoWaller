aa5cdf85333a23ca1dff21e4a85f95ebd536151a
gautamsood15 <gauty22@gmail.com>
2021-02-20 15:03:02 +0530

    getting current hour/time

--- autowaller.py
+++ autowaller.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python
 
 import os 
+from datetime import datetime
 
-os.system('gsettings set org.gnome.desktop.background picture-uri file:///home/gautamsood/Pictures/Wallpapers/firewatch_afternoon.jpg')
+now = datetime.now()
+
+current_time = now.strftime("%H:%M:%S")
+hr,mn,sec = str(current_time).split(":")
+
+hr = int(hr)
+
+
+
+
+
+
+
+#os.system('gsettings set org.gnome.desktop.background picture-uri file:///home/gautamsood/Pictures/Wallpapers/firewatch_afternoon.jpg')
 
