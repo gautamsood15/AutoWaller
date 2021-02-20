111d18f7f8435369e57ec53e840c1b278edbe415
gautamsood15 <gauty22@gmail.com>
2021-02-20 15:11:25 +0530

    create conditions for wallpaper change

--- autowaller.py
+++ autowaller.py
@@ -10,9 +10,18 @@ hr,mn,sec = str(current_time).split(":")
 
 hr = int(hr)
 
-
-
-
+if 5 > hr >= 0:
+	print("Late Night")
+elif 8 > hr >= 5:
+	print("Early Morning")
+elif 12 > hr >= 8:
+	print("Late Morning")
+elif 18 >= hr >= 12:
+	print("Afternoon")
+elif 23 >= hr > 18:
+	print("Early Night")
+else:
+	print("Invalid Hour")
 
 
 
